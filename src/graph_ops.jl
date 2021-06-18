ab333c8d92bcc82b4f2468dd155933b84e23d08b
LinjianMa <malinjianev@gmail.com>
2021-06-18 10:00:09 -0500

    Update interface based on AutoHOOT new API

--- src/graph_ops.jl
+++ src/graph_ops.jl
@@ -7,29 +7,31 @@ export
     # graph_transformer
     optimize,
     simplify,
-    rewrite_einsum_expr,
-    # graph_optimizer
     fuse_einsums,
+    # expr_generator
+    rewrite_einsum_expr,
     # graph_inv_optimizer
     optimize_inverse,
     prune_inv_node,
-    # graph_generator
+    # optimal_tree
     generate_optimal_tree,
     split_einsum,
     generate_optimal_tree_w_constraint,
     # graph_als_optimizer
     generate_sequential_optimal_tree
 
+const eingraph = PyNULL()
 const gops = PyNULL()
 
 function __init__()
+    copy!(eingraph, pyimport_conda("autohoot.einsum_graph", "autohoot"))
     copy!(gops, pyimport_conda("autohoot.graph_ops", "autohoot"))
 end
 
-@func_loader gops.graph_transformer optimize simplify rewrite_einsum_expr
-@func_loader gops.graph_optimizer fuse_einsums
+@func_loader eingraph.expr_generator rewrite_einsum_expr
+@func_loader gops.graph_transformer optimize simplify fuse_einsums
 @func_loader gops.graph_inv_optimizer optimize_inverse prune_inv_node
-@func_loader gops.graph_generator generate_optimal_tree split_einsum generate_optimal_tree_w_constraint
+@func_loader gops.optimal_tree generate_optimal_tree split_einsum generate_optimal_tree_w_constraint
 @func_loader gops.graph_als_optimizer generate_sequential_optimal_tree
 
 end
